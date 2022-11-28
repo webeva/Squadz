1413a0bc381e6d2a34fd48193033c24b2bfd2b45
unknown <web3eva@gmail.com>
2022-11-28 19:15:05 +0100

    mvp

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const app = express();
-const bodyParser = require('body-parser');
-app.use(cors({origin: '*'}));
+const bodyParser = require("body-parser");
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const http = require('http').createServer(app)
+const http = require("http").createServer(app);
 require("dotenv").config();
-
 
 //Set up the Express server
 
@@ -27,27 +26,35 @@ client.on("error", (err) => {
   console.log("Error in setting up Redis client: " + err);
 });
 
-app.post("/create-new-user", async function (req, res){
-  const request = req.body
+app.post("/create-new-user", async function (req, res) {
+  const request = req.body;
   if (!client.isOpen) {
     await client.connect();
   }
-  try{
-    let users = JSON.parse(await client.get(`${process.env.DEV_VAR}${request.Type}user${request.UID}`));
-    if(users){
-      console.log("Already exist")
-      res.status(200).send(`User with that ${request.Type} account already exists!`)
-     
-    }else{
-      const response = await client.set(process.env.DEV_VAR + request.Type + "user" + request.UID, JSON.stringify(request))
-      res.status(200).send("New user created")
+  try {
+    let users = JSON.parse(
+      await client.get(
+        `${process.env.DEV_VAR}${request.Type}user${request.UID}`
+      )
+    );
+    if (users) {
+      console.log("Already exist");
+      res
+        .status(200)
+        .send(`User with that ${request.Type} account already exists!`);
+    } else {
+      const response = await client.set(
+        process.env.DEV_VAR + request.Type + "user" + request.UID,
+        JSON.stringify(request)
+      );
+      res.status(200).send("New user created");
     }
-  }catch(error){
-    console.log(error)
-    return 
+  } catch (error) {
+    console.log(error);
+    return;
   }
-})
-app.get("/get-user-info/:id", async function(req, res){
+});
+app.get("/get-user-info/:id", async function (req, res) {
   if (!client.isOpen) {
     await client.connect();
   }
@@ -55,32 +62,42 @@ app.get("/get-user-info/:id", async function(req, res){
   let id = req.params.id;
   try {
     //Return chat messages if they were found
-
     return res.send(JSON.parse(await client.get(id)));
   } catch (error) {
     //Or else just return an empty object
     return {};
   }
-})
-
-app.post("/create-new-community", async function(req, res){
-  const request = req.body
+});
+app.get("/get-user-chats/:id", async function (req, res) {
   if (!client.isOpen) {
     await client.connect();
   }
-  try{
-    const response = await client.set(process.env.DEV_VAR + "community" + request.UID, JSON.stringify(request))
-  }catch(error){
-    return {}
+  //Url paramater to get the chat
+  let id = req.params.id;
+  const com = [];
+  try {
+    //Return chat messages if they were found
+
+    const comi = JSON.parse(await client.get(id)).CommunityList;
+    const community = comi.split("§");
+
+    community.map(async function (value) {
+      const response = await client.get(`$@community${value}`);
+      if (response) {
+        com.push(response);
+      }
+
+      if (com.length + 1 == community.length) {
+        return res.status(200).send(JSON.stringify(com));
+      }
+    });
+  } catch (error) {
+    //Or else just return an empty object
+    return {};
   }
-})
+});
 
-app.get("/join-new-community")
-
-app.get("/get-user-community/:id")
-app.get("/get-community/:id")
-
-app.get("/check-login/:id", async function(req,res){
+app.get("/get-community-info/:id", async function (req, res) {
   if (!client.isOpen) {
     await client.connect();
   }
@@ -88,38 +105,107 @@ app.get("/check-login/:id", async function(req,res){
   let id = req.params.id;
   try {
     //Return chat messages if they were found
-  
-    const response = JSON.parse(await client.get(id))
-    if(response){
-      res.status(200).send(true)
-      console.log("true")
-    }else{
-      res.status(200).send(false)
-      console.log("false")
+    console.log(id);
+    const community = await client.get(id);
+    return res.send(JSON.parse(community));
+  } catch (error) {
+    //Or else just return an empty object
+    return {};
+  }
+});
+
+app.post("/create-new-community", async function (req, res) {
+  const request = req.body;
+  if (!client.isOpen) {
+    await client.connect();
+  }
+  try {
+    const chatRoom = await client.set(
+      request.UID,
+      '[{"V":2,"community_id":"null","channel_id":"null","message_id":"TheOriginalMessage","sender_public_key":"null","message_type":"FIRST","images":null,"timestamp":1669563313457,"reply_id":null,"replies":null,"message":""}]'
+    );
+    const response = await client.set(
+      process.env.DEV_VAR + "community" + request.UID,
+      JSON.stringify(request)
+    );
+    res.status(200).send(response);
+  } catch (error) {
+    return {};
+  }
+});
+
+app.post("/join-new-community", async function (req, res) {
+  if (!client.isOpen) {
+    await client.connect();
+  }
+  try {
+    const request = req.body;
+
+    const response = JSON.parse(await client.get(request.UID));
+    response["CommunityList"] = response.CommunityList + `§${request.Id}`;
+    const result = await client.set(request.UID, JSON.stringify(response));
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+    return {};
+  }
+});
+
+app.get("/check-login/:id", async function (req, res) {
+  if (!client.isOpen) {
+    await client.connect();
+  }
+  //Url paramater to get the chat
+  let id = req.params.id;
+  try {
+    //Return chat messages if they were found
+
+    const response = JSON.parse(await client.get(id));
+    if (response) {
+      res.status(200).send(true);
+      console.log("true");
+    } else {
+      res.status(200).send(false);
+      console.log("false");
     }
   } catch (error) {
     //Or else just return an empty object
-    console.log("false")
+    console.log("false");
     res.status(200).send(false);
   }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
+app.get("/get-community-list", async function (req, res) {
+  if (!client.isOpen) {
+    await client.connect();
+  }
+  try {
+    //Return chat messages if they were found
+    const response = JSON.parse(await client.get("$@Discover"));
+    res.status(200).send(response);
+  } catch (error) {
+    //Or else just return an empty object
+    console.log("false");
+    res.status(200).send(false);
+  }
+});
+app.post("/add-community-list", async function (req, res) {
+  if (!client.isOpen) {
+    await client.connect();
+  }
+  try {
+    const request = req.body;
+    //Get the current chat messages in the form of an array
+    let chat_list = JSON.parse(await client.get("$@Discover"));
+    //Push the new message to the array
+    chat_list.push(request);
+    //Store the message in Redis
+    client.set("$@Discover", JSON.stringify(chat_list));
+    res.status(200).send(JSON.stringify(chat_list));
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+});
 //REST Api to get all the messages for a chat in Redis
 app.get("/get-messages/:id", async function (req, res) {
   if (!client.isOpen) {
@@ -135,15 +221,15 @@ app.get("/get-messages/:id", async function (req, res) {
     return {};
   }
 });
-app.get("*", (req, res)=>{
-  return res.send("server")
-})
+app.get("*", (req, res) => {
+  return res.send("server");
+});
 //Set up the socket.io serevr
 const io = require("socket.io")(http, {
   cors: {
-    origin: '*',
-  }
-})
+    origin: "*",
+  },
+});
 //Runs on connection
 io.on("connection", (socket) => {
   //Receive the name of the chat currently in
@@ -156,33 +242,39 @@ io.on("connection", (socket) => {
   //Runs whenever a message is sent
   //@params roomname string and message string
 
-  socket.on("send-chat-message", (room, message) => {
-    //Format the message object
-    const data = {
-      V: 2, //Version number (int)
-      community_id: room, //Community id (string)
-      channel_id: "development", //Channel id (string)
-      message_id: createMessageId(), //Message id (string)
-      sender_public_key: "testaccount", //User's public key (string)
-      message_type: "POST", //Message type (string)
-      images: null, //Images if any (array)
-      timestamp: Date.now(), //Curent timestamp (string)
-      reply_id: null, //Reply id if any (string)
-      replies: null, //Replies if any (array)
-      message: message, //Message (string)
-    };
-    //Save the message in our Redis cache database
-    saveMessage(data, id);
-    //Save the message in our DeSo database
-    axios
-      .post(
-        "https://97qtc0sfja.execute-api.us-east-1.amazonaws.com/default/SaharaMessageBot",
-        data
-      )
-      .then((response) => console.log(response.data));
-    //Broadcast the message to anybody connected to this room
-    socket.broadcast.to(room).emit("chat-message", message);
-  });
+  socket.on(
+    "send-chat-message",
+    (room, message, channel, image, user, name, profile) => {
+      //Format the message object
+
+      const data = {
+        V: 2, //Version number (int)
+        community_id: room, //Community id (string)
+        channel_id: channel, //Channel id (string)
+        message_id: createMessageId(), //Message id (string)
+        sender_public_key: user, //User's public key (string)
+        message_type: "POST", //Message type (string)
+        images: image, //Images if any (array)
+        timestamp: Date.now(), //Curent timestamp (string)
+        reply_id: null, //Reply id if any (string)
+        replies: null, //Replies if any (array)
+        message: message, //Message (string)
+        Name: name,
+        Profile: profile,
+      };
+      //Save the message in our Redis cache database
+      saveMessage(data, id);
+      //Save the message in our DeSo database
+      axios
+        .post(
+          "https://97qtc0sfja.execute-api.us-east-1.amazonaws.com/default/SaharaMessageBot",
+          data
+        )
+        .then((response) => console.log(response.data));
+      //Broadcast the message to anybody connected to this room
+      socket.broadcast.to(room).emit("chat-message", JSON.stringify(data));
+    }
+  );
 });
 //Function that saves the messages to our Redis cache database
 async function saveMessage(message, id) {
@@ -191,7 +283,6 @@ async function saveMessage(message, id) {
   if (!client.isOpen) {
     await client.connect();
   }
-
   try {
     //Get the current chat messages in the form of an array
     let chat_messages = JSON.parse(await client.get(id));
@@ -207,13 +298,13 @@ async function saveMessage(message, id) {
   }
 }
 
-
-function createMessageId(){
-    var length = 20,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?_.#@?/",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
+function createMessageId() {
+  var length = 20,
+    charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?_.#@?/",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
 }
